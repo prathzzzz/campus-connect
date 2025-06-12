c425130ef1145a32c20a444ed5983d025cdcb3ab
pratham <kariapratham@gmail.com>
2025-06-13 04:12:40 +0530

    feat: Create initial database tables

---
+++ database/migrations/2025_06_12_211229_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('roll_number')->unique();
+            $table->unsignedBigInteger('department_id');
+            $table->unsignedBigInteger('division_id');
+            $table->integer('batch');
+            $table->string('password');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+            $table->foreign('division_id')->references('id')->on('divisions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
