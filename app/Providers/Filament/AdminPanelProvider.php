40728097bf415af23d9c2242082aed0138d9bb4c
prathzzzz <kariapratham@gmail.com>
2025-06-16 01:06:57 +0530

    chore: add password reset functionality to admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('panel')
             ->path('panel')
             ->login()
+            ->passwordReset()
             ->profile(EditProfile::class)
             ->colors([
                 'primary' => Color::Amber,
