c9d21d3daf9a4d01d0cd1524c025923829bd8293
prathzzzz <kariapratham@gmail.com>
2025-06-16 19:23:26 +0530

    feat(users): update user resource and policies

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -56,6 +56,9 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
+        if ($user->id === $model->id) {
+            return false;
+        }
         return $user->can('delete-user');
     }
 
